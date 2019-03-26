17fc4e0b8cd1d497c27ec7bbbc702c5375681c71
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-25 16:17:23 -1000

    Add logos and about page

--- config.rb
+++ config.rb
@@ -39,7 +39,7 @@ activate :blog do |blog|
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.layout = "blog"
+  blog.layout = "blog_page"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
