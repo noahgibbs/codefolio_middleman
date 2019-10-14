4b468e7ad86bfe92266855e277dbc45559448a5a
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-10-14 08:36:09 +0100

    Update deploy script for CloudFlare, add Adam Cuppy interview to technique topics

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 git push || echo "Git push failed..."
-ssh noah@codefol.io ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && bundle exec middleman build"
+ssh noah@173.230.151.8 ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && bundle exec middleman build"
