68a64bdaeeecfd0ec16f52e1ba26c7344d2145f1
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-10-22 09:05:14 +0100

    Remove old comment from config.rb

--- config.rb
+++ config.rb
@@ -20,10 +20,6 @@ page '/about.html', layout: :blog_frame
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
-
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
