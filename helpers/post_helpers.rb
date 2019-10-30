a5b66d3ee9db43a75a1b124aebf720a73557c32d
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-10-30 09:40:30 +0000

    WIP on MST

--- helpers/post_helpers.rb
+++ helpers/post_helpers.rb
@@ -1,6 +1,7 @@
 module PostHelpers
-    def pull_quote(quote, attribution = nil, width: 8, direction: :right)
-        classes = "pull-quote col-#{width}"
+    def pull_quote(quote, attribution = nil, aside_classes: [], width: 8, direction: :right)
+        classes = aside_classes + ["pull-quote", "col-#{width}"]
+        classes = classes.join(" ")
         if direction == :right
           classes += " pull-right"
         elsif direction == :left
@@ -29,8 +30,8 @@ HTML
 HTML
     end
 
-    def aside_content(content, direction: :right, disappear_on_mobile: true)
-        classes = "inset"
+    def aside_content(content, div_classes: [], direction: :right, disappear_on_mobile: true)
+        classes = (div_classes + [ "inset" ]).join(" ")
         classes += " d-none d-md-block" if disappear_on_mobile
         if direction == :right
           classes += " pull-right"
@@ -47,16 +48,17 @@ HTML
 HTML
     end
 
-    def aside_image(url, alt:, width: nil, height: nil, caption: nil, direction: :right, disappear_on_mobile: false)
+    def aside_image(url, alt:, image_classes: [], div_classes: [], width: nil, height: nil, caption: nil, direction: :right, disappear_on_mobile: false)
         properties = { src: escape_html(url), alt: escape_html(alt) }
         properties[:width] = width.to_i.to_s unless width.nil?
         properties[:height] = height.to_i.to_s unless height.nil?
+        properties[:class] = image_classes.join(" ") unless image_classes.empty?
         props = properties.map { |name, val| "#{name}=\"#{val}\"" }.join(" ")
         image_html = <<HTML
 <img #{props} />
 #{caption}
 HTML
-        aside_content(image_html, direction: direction, disappear_on_mobile: disappear_on_mobile)
+        aside_content(image_html, div_classes: div_classes, direction: direction, disappear_on_mobile: disappear_on_mobile)
     end
 
 end
