05d8573c441c2757bad8dc1957e4a95cc7a8d9fd
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-11-07 20:24:47 +0000

    Formatting, lightbox and recent content updates.

--- helpers/post_helpers.rb
+++ helpers/post_helpers.rb
@@ -3,12 +3,13 @@ module PostHelpers
         classes = aside_classes + ["pull-quote", "col-#{width}"]
         classes = classes.join(" ")
         if direction == :right
-          classes += " pull-right"
+            classes += " pull-right"
         elsif direction == :left
-          classes += " pull-left"
+            classes += " pull-left"
         elsif direction == :none
+            # No-op
         else
-          raise "Unrecognized aside_image direction: #{direction.inspect}"
+            raise "Unrecognized pull_quote direction: #{direction.inspect}"
         end
         footer = ""
         if attribution
@@ -34,11 +35,13 @@ HTML
         classes = (div_classes + [ "inset" ]).join(" ")
         classes += " d-none d-md-block" if disappear_on_mobile
         if direction == :right
-          classes += " pull-right"
+            classes += " pull-right"
         elsif direction == :left
-          classes += " pull-left"
+            classes += " pull-left"
+        elsif direction == :none
+            # No-op
         else
-          raise "Unrecognized aside_image direction: #{direction.inspect}"
+            raise "Unrecognized aside_image direction: #{direction.inspect}"
         end
 
         <<HTML
@@ -61,4 +64,15 @@ HTML
         aside_content(image_html, div_classes: div_classes, direction: direction, disappear_on_mobile: disappear_on_mobile)
     end
 
+    def lightbox_image(url, thumbnail, alt:, image_classes: ["no-border", "img-fluid"], width: nil, height: nil)
+        properties = { src: escape_html(url), alt: escape_html(alt) }
+        properties[:width] = width.to_i.to_s unless width.nil?
+        properties[:height] = height.to_i.to_s unless height.nil?
+        properties[:class] = image_classes.join(" ") unless image_classes.empty?
+        props = properties.map { |name, val| "#{name}=\"#{val}\"" }.join(" ")
+        image_html = <<HTML
+<a href="#{escape_html(thumbnail)}" data-toggle="lightbox">
+<img #{props} /></a>
+HTML
+    end
 end
