0d31152f77d359cf4fcfc09c48a40b7f6198c6a9
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-02-28 11:28:01 +0000

    Revert "Fail in deploy if git push failed"
    This reverts commit 913cfa7105acd6de0f536aa33f9c21a3b3e9aff7.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-git push
+git push || echo "Git push failed..."
 ssh noah@173.230.151.8 ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && bundle exec middleman build"
