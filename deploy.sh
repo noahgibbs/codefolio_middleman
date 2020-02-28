16b1d123a1170c68c48de03d1759c7981406e7bb
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-02-28 11:29:23 +0000

    Try again...

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 set -e
 set -x
 
-# Git push will fail if there are no new commits, so we have to allow it.
-git push || echo "Git push failed..."
+git push
 ssh noah@173.230.151.8 ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && bundle exec middleman build"
