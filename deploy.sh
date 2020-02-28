913cfa7105acd6de0f536aa33f9c21a3b3e9aff7
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-02-28 11:27:51 +0000

    Fail in deploy if git push failed

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-git push || echo "Git push failed..."
+git push
 ssh noah@173.230.151.8 ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && bundle exec middleman build"
