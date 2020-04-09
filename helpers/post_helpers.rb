965fb7823340a9510a05924dfc1843411618cf11
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-04-09 08:53:26 +0100

    Initial conversion to use the Horace theme. Still lots of pages and posts that need converting.

--- helpers/post_helpers.rb
+++ helpers/post_helpers.rb
@@ -1,78 +1,38 @@
 module PostHelpers
-    def pull_quote(quote, attribution = nil, aside_classes: [], width: 8, direction: :right)
-        classes = aside_classes + ["pull-quote", "col-#{width}"]
-        classes = classes.join(" ")
+    def aside_image(url, alt:, image_classes: ["no-border"], width: nil, height: nil, direction: :none, caption: nil, disappear_on_mobile: false)
+        figure_classes = ""
         if direction == :right
-            classes += " pull-right"
+            figure_classes = "aside-right"
         elsif direction == :left
-            classes += " pull-left"
+            figure_classes = "aside-left"
         elsif direction == :none
-            # No-op
+            # Nothing
         else
-            raise "Unrecognized pull_quote direction: #{direction.inspect}"
-        end
-        footer = ""
-        if attribution
-            footer = <<HTML
-        <footer>
-          - #{attribution}
-        </footer
-HTML
+            raise "Unknown direction: #{direction.inspect}"
         end
 
-        <<HTML
-<aside class="#{classes}">
-    <blockquote>
-        <p>&quot;#{quote}&quot;</p>
-#{footer}
-    </blockquote>
-}
-</aside>
-HTML
+        image_classes += ["d-none", "d-md-block"] if disappear_on_mobile
+        partial("image-caption", locals: {
+            imageurl: url,
+            alt: alt,
+            width: width,
+            height: height,
+            imageclasses: image_classes.join(" "),
+            figureclasses: figure_classes,
+            caption: caption,
+            direction: direction,
+        })
     end
 
-    def aside_content(content, div_classes: [], direction: :right, disappear_on_mobile: true)
-        classes = (div_classes + [ "inset" ]).join(" ")
-        classes += " d-none d-md-block" if disappear_on_mobile
-        if direction == :right
-            classes += " pull-right"
-        elsif direction == :left
-            classes += " pull-left"
-        elsif direction == :none
-            # No-op
-        else
-            raise "Unrecognized aside_image direction: #{direction.inspect}"
-        end
-
-        <<HTML
-<div class=#{classes.inspect}>
-  #{content}
-</div>
-HTML
-    end
-
-    def aside_image(url, alt:, image_classes: ["no-border"], div_classes: [], width: nil, height: nil, caption: nil, direction: :right, disappear_on_mobile: false)
-        properties = { src: escape_html(url), alt: escape_html(alt) }
-        properties[:width] = width.to_i.to_s unless width.nil?
-        properties[:height] = height.to_i.to_s unless height.nil?
-        properties[:class] = image_classes.join(" ") unless image_classes.empty?
-        props = properties.map { |name, val| "#{name}=\"#{val}\"" }.join(" ")
-        image_html = <<HTML
-<img #{props} /><br/>
-#{caption}
-HTML
-        aside_content(image_html, div_classes: div_classes, direction: direction, disappear_on_mobile: disappear_on_mobile)
-    end
-
-    def lightbox_image(url, thumbnail, alt:, image_classes: ["no-border", "img-fluid"], width: nil, height: nil)
-        properties = { src: escape_html(url), alt: escape_html(alt) }
-        properties[:width] = width.to_i.to_s unless width.nil?
-        properties[:height] = height.to_i.to_s unless height.nil?
-        properties[:class] = image_classes.join(" ") unless image_classes.empty?
-        props = properties.map { |name, val| "#{name}=\"#{val}\"" }.join(" ")
-        image_html = <<HTML
-<a href="#{escape_html(thumbnail)}" data-toggle="lightbox">
-<img #{props} /></a>
-HTML
-    end
+#    def lightbox_image(url, thumbnail, alt:, image_classes: ["no-border", "img-fluid"], width: nil, height: nil)
+#        properties = { src: escape_html(url), alt: escape_html(alt) }
+#        properties[:width] = width.to_i.to_s unless width.nil?
+#        properties[:height] = height.to_i.to_s unless height.nil?
+#        properties[:class] = image_classes.join(" ") unless image_classes.empty?
+#        props = properties.map { |name, val| "#{name}=\"#{val}\"" }.join(" ")
+#        image_html = <<HTML
+#<a href="#{escape_html(thumbnail)}" data-toggle="lightbox">
+#<img #{props} /></a>
+#HTML
+#    end
 end
