304644bd7002b740d1d44e4eb35e74cf28642875
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-04-23 13:37:02 +0100

    More debugging and refactoring

--- assets/process.rb
+++ assets/process.rb
@@ -6,6 +6,7 @@
 
 require "json"
 require "pathname"
+require "fileutils"
 
 Dir.chdir(__dir__ + "/..")
 
@@ -27,9 +28,16 @@ def image_w_h(filename)
   return w, h
 end
 
-def handle_output_size(size_name, asset_file, max_width)
+def ensure_symlink(to, from)
+  unless File.symlink?(from) && File.readlink(from) == to
+    FileUtils.rm_f(from)
+    File.symlink(to, from)
+  end
+end
+
+def handle_output_size(size_name, asset_file, max_width, force_ext: nil)
   ext = File.extname(asset_file)
-  output_ext = ext.downcase.sub("jpeg", "jpg")
+  output_ext = force_ext ? force_ext : ext.downcase.sub("jpeg", "jpg")
   dir = File.dirname(asset_file)
   basename = File.basename(asset_file, ext)
 
@@ -40,11 +48,10 @@ def handle_output_size(size_name, asset_file, max_width)
     system("convert #{orig_filename} -auto-orient -unsharp 0x.5 -background black -alpha remove -thumbnail #{max_width}x#{max_width} #{output_path}") || raise("Could not convert image to #{size_name}: #{orig_filename.inspect}!")
   end
   output_w, output_h = image_w_h(output_path)
-  output_dims_path = output_path.sub("_output#{output_ext}", "_#{output_w}_#{output_h}#{output_ext}")
-  output_dims_url = output_url.sub("_output#{output_ext}", "_#{output_w}_#{output_h}#{output_ext}")
-  unless File.symlink?(output_dims_path)
-    File.symlink("#{basename}_#{size_name}#{output_ext}", output_dims_path)
-  end
+  output_dims_path = output_path.sub("_#{size_name}#{output_ext}", "_#{size_name}_#{output_w}_#{output_h}#{output_ext}")
+  output_dims_url = output_url.sub("_#{size_name}#{output_ext}", "_#{size_name}_#{output_w}_#{output_h}#{output_ext}")
+
+  ensure_symlink("#{basename}_#{size_name}#{output_ext}", output_dims_path)
 
   {
     "#{size_name}_path" => output_path,
@@ -79,21 +86,19 @@ Dir["assets/*.json"].each do |asset_file|
 
   full_path = Pathname.new("source/img/#{dir}/#{basename}_full#{output_ext}").cleanpath.to_s
   full_url = Pathname.new("/img/#{dir}/#{basename}_full#{output_ext}").cleanpath.to_s
-  unless File.symlink?(full_path)
-    sym_path = Pathname.new("../orig/#{asset["file"]}").cleanpath.to_s
-    File.symlink(sym_path, full_path)
-  end
+  sym_path = Pathname.new(orig_filename).relative_path_from("source/img/#{dir}").to_s
+  ensure_symlink(sym_path, full_path)
+
   full_w, full_h = image_w_h(full_path)
   full_dims_path = full_path.sub("_full#{output_ext}", "_#{full_w}_#{full_h}#{output_ext}")
-  unless File.symlink?(full_dims_path)
-    File.symlink("#{basename}_full#{output_ext}", full_dims_path)
-  end
+  full_dims_url = full_url.sub("_full#{output_ext}", "_#{full_w}_#{full_h}#{output_ext}")
+  ensure_symlink("#{basename}_full#{output_ext}", full_dims_path)
 
   # Create an aside-sized small-scale image called FILENAME_aside.EXT and a symlink with dimensions in the filename
   aside_props = handle_output_size("aside", asset["file"], 216)
 
   # Create a thumbnail called FILENAME_thumb.png and a symlink to it with dimensions in the filename
-  thumb_props = handle_output_size("thumbnail", asset["file"], 100)
+  thumb_props = handle_output_size("thumbnail", asset["file"], 100, force_ext: ".png")
 
   generated_info = {
     orig_path: orig_filename,
