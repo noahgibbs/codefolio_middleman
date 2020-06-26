fa3d460e53a9e9adb3d3772659c91eaa41268097
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-26 20:52:57 +0100

    Dubious hack to for new deployment environment

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 git push
-ssh noah@173.230.151.8 ". ~/.bash_profile && cd checkouts/codefolio_middleman && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
+ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/codefolio_middleman && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
