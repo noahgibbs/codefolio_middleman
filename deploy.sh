d30d73daf6ed23606e32ebd5498736b2a0cb3bac
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-06-09 23:17:34 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-set -x
+echo "Don't need this any more. These days it's all hosted off the repo.
 
-git push
-ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/codefolio_middleman && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
+#git push
+#ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/codefolio_middleman && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
