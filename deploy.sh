746369b8d09e0f13324636dd43e351ebad227438
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-08-01 19:30:22 +0100

    Fix typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Don't need this any more. These days it's all hosted off the repo.
+echo "Don't need this any more. These days it's all hosted off the repo."
 
 #git push
 #ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/codefolio_middleman && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
